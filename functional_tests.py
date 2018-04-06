bda636a1a1d5498787c4b1a6966c8a817084e616
Eric Ervin <ericcervin@gmail.com>
2018-04-06 00:31:03 -0400

    first commit

---
+++ functional_tests.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+  def setUp(self):
+      self.browser = webdriver.Firefox()
+
+  def tearDown(self):
+      self.browser.quit()
+  
+  def test_ericervin_dot_org(self):
+    self.browser.get('http://ericervin.org')
+    self.assertIn('Eric Ervin Dot Org', self.browser.title)
+
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('Eric Ervin Dot Org', header_text)
+
+  def test_ericervin_dot_org_destiny(self):
+    self.browser.get('http://ericervin.org/destiny')
+    self.assertIn('Destiny', self.browser.title)
+
+    #add a properly sized header
+
+  def test_ericervin_dot_org_destiny_cards(self):
+    self.browser.get('http://ericervin.org/destiny/cards?')
+    self.assertIn('Destiny', self.browser.title)
+
+    #add code that counts rows in table.
+    #maybe compare with output in ericervin_dot_com
+
+  
+    
+  def test_ericervin_dot_org_gematria(self):
+    self.browser.get('http://ericervin.org/gematria')
+    #add a proper title
+
+    #add a properly sized header
+
+    #give input fields proper ids that you can find
+    #then add Keys input for field and submit
+    
+  def test_ericervin_dot_com(self):
+    self.browser.get('http://ericervin.com')
+    self.assertIn('Eric Ervin Dot Com', self.browser.title)
+
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('Eric Ervin Dot Com', header_text)
+
+  def test_ericcervin_dot_github_dot_io(self):
+    self.browser.get('http://ericcervin.github.io')
+    self.assertIn('ericcervin.github.io', self.browser.title)
+
+  def test_noiselife_dot_org(self):
+    self.browser.get('http://noiselife.org')
+    self.assertIn('noiselife-dot-org', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
